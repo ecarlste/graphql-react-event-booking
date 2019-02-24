1e4789d97960a00302771f371f4e06b7f2bba5eb
Erik S. Carlsten <ecarlste@gmail.com>
2019-02-23 19:07:42 -0700

    Added empty graphql middleware call

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-});
+app.use('/graphql', graphqlHttp({
+    schema: null,
+    rootValue: {}
+}));
 
 app.listen(process.env.NOVE_ENV || 3000);
