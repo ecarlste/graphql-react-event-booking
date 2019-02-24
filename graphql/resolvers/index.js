ecf92eca7f23cd7e8fdf1cb628bc4e1f6dec26ee
Erik S. Carlsten <ecarlste@gmail.com>
2019-02-24 15:49:25 -0700

    Added helper to convert mongodb date to ISO string

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,12 +3,13 @@ const bcrypt = require('bcryptjs');
 const Booking = require('../../models/booking');
 const Event = require('../../models/event');
 const User = require('../../models/user');
+const { dateToString } = require('../../helpers/date');
 
 const transformEvent = event => {
     return {
         ...event._doc,
         _id: event.id,
-        date: new Date(event._doc.date).toISOString(),
+        date: dateToString(event._doc.date),
         creator: user.bind(this, event.creator)
     }
 };
@@ -70,8 +71,8 @@ const resolvers = {
                     _id: booking.id,
                     user: user.bind(this, booking._doc.user),
                     event: singleEvent.bind(this, booking._doc.event),
-                    createdAt: new Date(booking._doc.createdAt).toISOString(),
-                    updatedAt: new Date(booking._doc.updatedAt).toISOString()
+                    createdAt: dateToString(booking._doc.createdAt),
+                    updatedAt: dateToString(booking._doc.updatedAt)
                 }
             })
         } catch (err) {
@@ -145,8 +146,8 @@ const resolvers = {
         return {
             ...result._doc,
             _id: result.id,
-            createdAt: new Date(booking._doc.createdAt).toISOString(),
-            updatedAt: new Date(booking._doc.updatedAt).toISOString()
+            createdAt: dateToString(booking._doc.createdAt),
+            updatedAt: dateToString(booking._doc.updatedAt)
         }
     },
     cancelBooking: async args => {
