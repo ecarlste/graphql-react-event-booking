1b6de5f42d5e429ceadee1b9c4d5340247adf167
Erik S. Carlsten <ecarlste@gmail.com>
2019-02-24 17:47:16 -0700

    Added login resolver to auth

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,34 +1,58 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../../models/user');
 
-const BookingResolvers = {
+const AuthResolvers = {
     createUser: async args => {
-    try {
-        const user = await User.findOne({ email: args.userInput.email })
+        try {
+            const user = await User.findOne({ email: args.userInput.email })
 
-        if (user) {
-            throw new Error('User already exists.');
+            if (user) {
+                throw new Error('User already exists.');
+            }
+
+            const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+
+            const createdUser = new User({
+                email: args.userInput.email,
+                password: hashedPassword
+            });
+
+            const result = await createdUser.save();
+
+            return {
+                ...result._doc,
+                password: null,
+                _id: result.id
+            };
+        } catch (err) {
+            throw err;
+        }
+    },
+    login: async ({email, password}) => {
+        const user = await User.findOne({email: email});
+        if (!user) {
+            throw new Error('Unable to authenticate!');
         }
 
-        const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+        const isCorrectPassword = await bcrypt.compare(password, user.password);
+        if (!isCorrectPassword) {
+            throw new Error('Unable to authenticate!');
+        }
 
-        const createdUser = new User({
-            email: args.userInput.email,
-            password: hashedPassword
-        });
-
-        const result = await createdUser.save();
+        const token = jwt.sign(
+            { userId: user.id, email: user.email },
+            'somesupersecretkey :-P',
+            { expiresIn: '1h' }
+        );
 
         return {
-            ...result._doc,
-            password: null,
-            _id: result.id
-        };
-    } catch (err) {
-        throw err;
+            userId: user.id,
+            token: token,
+            tokenExpiration: 1
+        }
     }
 }
-}
 
-module.exports = BookingResolvers;
+module.exports = AuthResolvers;
