e70518a9332c1964109bd0befd20bedb6231f799
Erik S. Carlsten <ecarlste@gmail.com>
2019-02-25 18:36:56 -0700

    Wired up auth form to a submitHandler

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -3,21 +3,42 @@ import React, { Component } from 'react';
 import './Auth.css';
 
 class AuthPage extends Component {
+    constructor(props) {
+        super(props);
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        
+        const email = this.emailEl.current.value;
+        const password = this.passwordEl.current.value;
+
+        if (email.trim().length === 0 || password.trim().length === 0) {
+            return;
+        }
+
+        console.log(email, password);
+    }
+
     render() {
-        return <form className="auth-form">
-            <div className="form-control">
-                <label htmlFor="email">E-Mail</label>
-                <input type="email" id="email" />
-            </div>
-            <div className="form-control">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
-            </div>
-            <div className="form-actions">
-                <button type="submit">Submit</button>
-                <button type="button">Sign Up</button>
-            </div>
-        </form>
+        return (
+            <form className="auth-form" onSubmit={this.submitHandler}>
+                <div className="form-control">
+                    <label htmlFor="email">E-Mail</label>
+                    <input type="email" id="email" ref={this.emailEl} />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" ref={this.passwordEl} />
+                </div>
+                <div className="form-actions">
+                    <button type="submit">Submit</button>
+                    <button type="button">Sign Up</button>
+                </div>
+            </form>
+        )
     }
 }
 
