ab863a1fdf17378c556e8abf89a15071b1bc7e31
Erik S. Carlsten <ecarlste@gmail.com>
2019-02-25 21:15:49 -0700

    Implemented first go at auth context provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,49 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import './App.css';
+import AuthContext from './context/auth-context';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
 import MainNavigation from './components/Navigation/MainNavigation';
 
 class App extends Component {
+  state = {
+    token: null,
+    userId: null
+  }
+
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token: token, userId: userId });
+  };
+
+  logout = () => {
+    this.setState({ token: null, userId: null });
+  };
+
   render() {
     return (
       <BrowserRouter>
         <React.Fragment>
-          <MainNavigation />
-          <main className="main-content">
-            <Switch>
-              <Redirect from="/" to="/auth" exact />
-              <Route path="/auth" component={AuthPage} />
-              <Route path="/events" component={EventsPage} />
-              <Route path="/bookings" component={BookingsPage} />
-            </Switch>
-          </main>
+          <AuthContext.Provider
+            value={{
+              token: this.state.token,
+              userId: this.state.userId,
+              login: this.login,
+              logout: this.logout
+            }}
+          >
+
+            <MainNavigation />
+            <main className="main-content">
+              <Switch>
+                <Redirect from="/" to="/auth" exact />
+                <Route path="/auth" component={AuthPage} />
+                <Route path="/events" component={EventsPage} />
+                <Route path="/bookings" component={BookingsPage} />
+              </Switch>
+            </main>
+          </AuthContext.Provider>
         </React.Fragment>
       </BrowserRouter>
     );
