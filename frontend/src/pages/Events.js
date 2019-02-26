fea694ce1889723d191a6777a142508189d2551d
Erik S. Carlsten <ecarlste@gmail.com>
2019-02-25 23:10:41 -0700

    Built initial cancel/confirm modal for create event

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 
 import './Events.css';
+import Backdrop from '../components/Backdrop/Backdrop';
+import Modal from '../components/Modal/Modal';
 
 class EventsPage extends Component {
+    state = {
+        creating: false
+    };
+
+    startCreateEventHandler = () => {
+        this.setState({creating: true});
+    };
+
+    modalCancelHandler = () => {
+        this.setState({creating: false});
+    };
+
+    modalConfirmHandler = () => {
+        this.setState({creating: false});
+    };
+
     render() {
         return (
-            <div className="events-control">
-                <p>Share your own events!</p>
-                <button className="btn">Create Event</button>
-            </div>
+            <React.Fragment>
+            {this.state.creating && (
+                <React.Fragment>
+                    <Backdrop />
+                    <Modal title="Add Event" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
+                        <p>Modal Content</p>
+                    </Modal>
+                </React.Fragment>
+            )}
+                <div className="events-control">
+                    <p>Share your own events!</p>
+                    <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
+                </div>
+            </React.Fragment>
         );
     }
 }
