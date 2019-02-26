bd21fccae0d2dcb86c0ed00d034c89e0c08bf5c4
Erik S. Carlsten <ecarlste@gmail.com>
2019-02-26 10:57:17 -0700

    Added create event form inputs inside the new Modal

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -28,7 +28,24 @@ class EventsPage extends Component {
                 <React.Fragment>
                     <Backdrop />
                     <Modal title="Add Event" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
-                        <p>Modal Content</p>
+                        <form>
+                            <div className="form-control">
+                                <label htmlFor="title">Title</label>
+                                <input type="text" id="title"></input>
+                            </div>
+                            <div className="form-control">
+                                <label htmlFor="price">Price</label>
+                                <input type="number" id="price"></input>
+                            </div>
+                            <div className="form-control">
+                                <label htmlFor="date">Date</label>
+                                <input type="date" id="date"></input>
+                            </div>
+                            <div className="form-control">
+                                <label htmlFor="description">Title</label>
+                                <textarea id="description" rows="4" />
+                            </div>
+                        </form>
                     </Modal>
                 </React.Fragment>
             )}
