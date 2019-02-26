549e2de51d8b61ea7dd08672f60fa05524d5d598
Erik S. Carlsten <ecarlste@gmail.com>
2019-02-26 16:24:54 -0700

    Wired up events list to the graphql api

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -7,7 +7,8 @@ import Modal from '../components/Modal/Modal';
 
 class EventsPage extends Component {
     state = {
-        creating: false
+        creating: false,
+        events: []
     };
 
     static contextType = AuthContext;
@@ -20,9 +21,55 @@ class EventsPage extends Component {
         this.descriptionElRef = React.createRef();
     }
 
+    componentDidMount() {
+        this.fetchEvents();
+    }
+
     startCreateEventHandler = () => {
         this.setState({creating: true});
     };
+
+    fetchEvents() {
+        let requestBody = {
+            query: `
+                query {
+                    events {
+                        _id
+                        title
+                        price
+                        date
+                        description
+                        creator {
+                            _id
+                            email
+                        }
+                    }
+                }
+            `
+        };
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => {
+            if (res.status !== 200 && res.status !== 201) {
+                throw new Error('Failed!');
+            }
+            return res.json();
+        })
+        .then(resData => {
+            const events = resData.data.events;
+            this.setState({events: events});
+            console.log('finished setting state');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
 
     modalCancelHandler = () => {
         this.setState({creating: false});
@@ -89,7 +136,7 @@ class EventsPage extends Component {
             return res.json();
         })
         .then(resData => {
-            console.log(resData);
+            this.fetchEvents();
         })
         .catch(err => {
             console.log(err);
@@ -97,41 +144,50 @@ class EventsPage extends Component {
     };
 
     render() {
+        console.log(this.state.events);
+
+        const eventList = this.state.events.map(event => {
+            return (
+                <li key={event._id} className="events__list-item">{event.title}</li>
+            );
+        });
+
         return (
             <React.Fragment>
-            {this.state.creating && (
-                <React.Fragment>
-                    <Backdrop />
-                    <Modal title="Add Event" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
-                        <form>
-                            <div className="form-control">
-                                <label htmlFor="title">Title</label>
-                                <input type="text" id="title" ref={this.titleElRef}></input>
-                            </div>
-                            <div className="form-control">
-                                <label htmlFor="price">Price</label>
-                                <input type="number" id="price" ref={this.priceElRef}></input>
-                            </div>
-                            <div className="form-control">
-                                <label htmlFor="date">Date</label>
-                                <input type="datetime-local" id="date" ref={this.dateElRef}></input>
-                            </div>
-                            <div className="form-control">
-                                <label htmlFor="description">Title</label>
-                                <textarea id="description" rows="4" ref={this.descriptionElRef} />
-                            </div>
-                        </form>
-                    </Modal>
-                </React.Fragment>
-            )}
-            {this.context.token && (
-                <div className="events-control">
-                    <p>Share your own events!</p>
-                    <button className="btn" onClick={this.startCreateEventHandler}>
-                        Create Event
-                    </button>
-                </div>
-            )}
+                {this.state.creating && (
+                    <React.Fragment>
+                        <Backdrop />
+                        <Modal title="Add Event" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
+                            <form>
+                                <div className="form-control">
+                                    <label htmlFor="title">Title</label>
+                                    <input type="text" id="title" ref={this.titleElRef}></input>
+                                </div>
+                                <div className="form-control">
+                                    <label htmlFor="price">Price</label>
+                                    <input type="number" id="price" ref={this.priceElRef}></input>
+                                </div>
+                                <div className="form-control">
+                                    <label htmlFor="date">Date</label>
+                                    <input type="datetime-local" id="date" ref={this.dateElRef}></input>
+                                </div>
+                                <div className="form-control">
+                                    <label htmlFor="description">Title</label>
+                                    <textarea id="description" rows="4" ref={this.descriptionElRef} />
+                                </div>
+                            </form>
+                        </Modal>
+                    </React.Fragment>
+                )}
+                {this.context.token && (
+                    <div className="events-control">
+                        <p>Share your own events!</p>
+                        <button className="btn" onClick={this.startCreateEventHandler}>
+                            Create Event
+                        </button>
+                    </div>
+                )}
+                <ul className="events__list">{eventList}</ul>
             </React.Fragment>
         );
     }
